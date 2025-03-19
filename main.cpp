27aa7138bf2bab9fa6c926f3cf854aceb4ed8eb8
Ch-Ke <59898427+Ch-Ke@users.noreply.github.com>
2025-03-19 10:30:51 +1030

    Implemented RLE algorithm in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
 #include <string>
 
-std::string encode(std::string inputString);
+std::string encode(std::string &inputString);
 
 int main (int argc, char *argv[]) {
   std::string testString = "AAAABBBCCDAA";
   std::string encodedString = encode(testString);
+  std::cout << encodedString << std::endl;
   return 0;
 }
 
-std::string encode(std::string inputString){
-  std::cout << "Hello, world!" << std::endl;
-  return "";
+// Encode string with RLE algorithm and return encoded string
+std::string encode(std::string &inputString){
+  std::string encodedString = "";
+  int n = inputString.size();
+  for(int i = 0; i < n; i++){
+    int count = 1;
+    while(i + 1 < n && inputString[i] == inputString[i+1]){
+      count++;
+      i++;
+    }
+    encodedString += std::to_string(count);
+    encodedString.push_back(inputString[i]);
+  }
+  return encoded;
 }
